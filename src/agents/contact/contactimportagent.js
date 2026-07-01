5053f7fcd6ec727b3fb3035b9f53c9773c331b71
ArunPlumb5 <arunamr@decisive.in>
2026-07-01 17:18:43 +0530

    add

---
+++ src/agents/contact/contactimportagent.js
@@ -0,0 +1,40 @@
+  import {
+    createAgent
+  } from "../../utils/agent.factory.js";
+
+  export async function executeContactImportAgent({
+    model,
+    tools,
+    history,
+    accountId,
+    session
+  }) {
+    const agent = createAgent({
+      module: "contactimport",
+      model,
+      tools,
+      accountId,
+      session
+    });
+
+    let uploadedFileContext = "SESSION UPLOADED FILES: NONE";
+
+      if (session?.uploadedFile?.length > 0) {
+      uploadedFileContext = `
+      SESSION UPLOADED FILES:
+      ${session.uploadedFile
+      .map(file => `- ${file.fileName}`)
+      .join("\n")}
+      `;
+      }
+
+    return await agent.invoke({
+      messages: [
+        {
+          role: "system",
+          content: uploadedFileContext
+        },
+        ...history
+      ]
+    });
+  }

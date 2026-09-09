6b646ca1fa5dee1a675ebb779b97ab913ab8b122
ksranjith9952 <ranjith.ks@decisive.in>
2026-09-09 10:26:29 +0530

    comit

--- src/utils/agent.factory.js
+++ src/utils/agent.factory.js
@@ -52,7 +52,7 @@ import { WHATSAPPCAMPAIGN_PROMPT } from "../prompts/whatsapp/whatsappcampaign.pr
 
 import { WEBPUSHTEMPLATE_PROMPT } from "../prompts/webpush/webpushtemplate.prompt.js";
 import { WEBPUSHTEST_PROMPT } from "../prompts/webpush/webpushtest.prompt.js";
-
+import {WEBPUSHCAMPAIGN_PROMPT} from "../prompts/webpush/webpushcampaign.prompt.js";
 function getPrompt(module) {
   const prompts = {
     knowledge: KNOWLEDGE_PROMPT,
